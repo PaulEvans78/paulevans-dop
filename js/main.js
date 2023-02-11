6b00dfba9926409ca101b7ecfa6c1890000fee73
Paul Evans <84741537+PaulEvans78@users.noreply.github.com>
2023-02-11 14:51:53 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// Close Hamburger menu
+
+document.querySelectorAll('#indexContact').forEach(l=>l.addEventListener('click',e => document.getElementById('menu__toggle').click())); 
+
+
+// Video Player 
+
+  function videoUrl(x){
+  document.getElementById("vidSlider").src = x;
+  }
+  
+
+
+
+
